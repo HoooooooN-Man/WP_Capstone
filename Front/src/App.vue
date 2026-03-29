97f83dff6c2e3f5182020a1283dc4a767b6df3f7
3JINU247 <wnwlsdn1482@gmail.com>
2026-03-30 00:36:54 +0900

    Revert "front_test"
    This reverts commit 9fb651958f11154bf6a677b31cf9a5342ac7f768.

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,15 +1,7 @@
-<template>
-  <div id="app">
-    <LoginView />
-  </div>
-</template>
-
-<script setup>
-// 윈도우 환경에서도 이 상대 경로가 가장 안전합니다.
-import LoginView from './components/LoginView.vue'
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-/* 화면 여백 제거 */
-body { margin: 0; padding: 0; overflow: hidden; }
-</style>
+<template>
+  <HelloWorld />
+</template>
